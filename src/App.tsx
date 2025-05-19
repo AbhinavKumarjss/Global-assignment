ad77dd0d5fb8a2f9108daad3294bbf1a8ea2c1ba
Abhinav Kumar <22cs035@jssaten.ac.in>
2025-05-19 13:26:21 +0530

    Height-issue-fixed

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import Footer from "./components/Footer/Footer";
 
 export default function App() {
   return (
-    <div className="flex w-full max-h-screen bg-white dark:bg-[var(--color-darkgray)]">
+    <div className="flex w-full h-screen bg-white dark:bg-[var(--color-darkgray)]">
 
 
           <Router>
@@ -31,7 +31,7 @@ export default function App() {
               <Route path='/upload' element={
                 <>
                   <Sidebar />
-                  <div className="page-wrapper-sub w-full max-h-screen dark:bg-[var(--color-darkgray)]">
+                  <div className="page-wrapper-sub w-full h-full dark:bg-[var(--color-darkgray)]">
                     <div className="page-wrapper-sub flex flex-col w-full h-full dark:bg-darkgray">
                       <Navbar />
                       <Upload />
